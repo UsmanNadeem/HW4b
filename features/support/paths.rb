a4350177bf459ed971b36ffee3428b001446ab6a
UsmanNadeem <usmanone@gmail.com>
2015-04-12 14:19:58 -0300

    view

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -21,12 +21,12 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-    
-    # when /^the edit page for "(.*)"$/
-    #   edit_movie_path Movie.find_by_title($1)
-    # when /^the details page for "(.*)"$/
-    #   movie_path Movie.find_By_title($1)
-    # when /^the Similar Movies page for "(.*)"$/
+
+    when /^the edit page for "(.*)"$/i
+      edit_movie_path Movie.find_by_title($1)
+    when /^the details page for "(.*)"$/i
+      movie_path(Movie.find_by_title($1))
+    # when /^the Similar Movies page for "(.*)"$/i
     #   same_Director_path Movie.find_by_title($1)
 
     else
